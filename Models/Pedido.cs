31897524567fb117d37d922fd082433ca59d756e
Valeria Nastar Cordoba <valeria.nastar@siigo.com>
2025-09-17 12:07:23 -0500

    feat: dockerizar app

--- Models/Pedido.cs
+++ Models/Pedido.cs
@@ -4,6 +4,9 @@
     {
         public int id { get; set; }
         public required string cliente { get; set; }
+        public required string producto { get; set; }
+        public int cantidad { get; set; }
+        public DateTime fecha { get; set; }
         public required decimal total { get; set; }
     }
 }
